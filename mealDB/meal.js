355515bff06a450b5265a0a7afd1d6e6d16a41ff
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-16 11:56:09 +0600

    display data from api

--- mealDB/meal.js
+++ mealDB/meal.js
@@ -17,8 +17,8 @@ const displaySearchResult = meals => {
         <div class="card h-100">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
         </div>
       </div>`;
       searchResult.appendChild(div);
